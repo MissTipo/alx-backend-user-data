47516526762dbb083d0d33863b69addef54744a3
MissTipo <dorine.a.tipo@gmail.com>
2023-05-03 09:58:57 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,7 +17,17 @@ class BasicAuth(Auth):
             return None
         return authorization_header.split(' ')[1] if len(
             authorization_header.split(' ')) > 1 else None
-        '''else:
-            if len(authorization_header.split(' ')) > 1:
-                return authorization_header.split(' ')[1]
-            return None'''
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Return the decoded value of a Base64 string."""
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            decoded_bytes = base64.b64decode(base64_bytes)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except BaseException:
+            return None
