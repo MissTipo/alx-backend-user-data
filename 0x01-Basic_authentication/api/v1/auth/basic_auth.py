457a437214ec557db99369c42d834ab974baa722
MissTipo <dorine.a.tipo@gmail.com>
2023-05-03 09:19:58 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 """Basic Auth."""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Inherits from Auth."""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Return Base64 part of Authorization."""
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+        elif not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1] if len(
+            authorization_header.split(' ')) > 1 else None
+        '''else:
+            if len(authorization_header.split(' ')) > 1:
+                return authorization_header.split(' ')[1]
+            return None'''
