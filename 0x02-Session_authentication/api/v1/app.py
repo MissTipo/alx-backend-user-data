dc32452da9a917ddf6f236a089c118a2208488ce
MissTipo <dorine.a.tipo@gmail.com>
2023-05-04 13:43:26 +0300

    fix typo

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -26,23 +26,23 @@ else:
     auth = None
 
 
+excluded_paths = [
+    '/api/v1/status/',
+    '/api/v1/unauthorized/',
+    '/api/v1/forbidden/']
+
+
 @app.before_request
 def before_request():
     """Handle before request"""
-    if auth is None:
-        return
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-    else:
-        request.current_user = auth.current_user(request)
+    if auth:
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+            else:
+                request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
