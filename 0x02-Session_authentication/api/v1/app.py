61000d94ab0d05b246c4f90767cc21f2101f4d3b
MissTipo <dorine.a.tipo@gmail.com>
2023-05-07 04:22:25 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,7 +32,8 @@ else:
 excluded_paths = [
     '/api/v1/status/',
     '/api/v1/unauthorized/',
-    '/api/v1/forbidden/']
+    '/api/v1/forbidden/'
+    '/api/v1/auth_session/login/']
 
 
 @app.before_request
@@ -44,6 +45,9 @@ def before_request():
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
+                abort(401)
             else:
                 request.current_user = auth.current_user(request)
 
