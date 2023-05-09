0f1f66340525524b15c32802a0d40a2f960f663f
MissTipo <dorine.a.tipo@gmail.com>
2023-05-09 16:23:40 +0300

    User authentication

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Authenticates the user."""
+import bcrypt
+
+def _hash_password(password):
+    """Hashes a password."""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
