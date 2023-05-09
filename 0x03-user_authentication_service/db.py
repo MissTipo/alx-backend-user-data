c688dad7849349b512709fbbd2a9789a391685ba
MissTipo <dorine.a.tipo@gmail.com>
2023-05-09 16:37:14 +0300

    validate pycodestyle

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Create user."""
         try:
-            user = User(email=email, hashed_password=hashed_password )
+            user = User(email=email, hashed_password=hashed_password)
             self._session.add(user)
             self._session.commit()
         except Exception:
@@ -55,4 +55,3 @@ class DB:
             setattr(user, key, value)
         self._session.commit()
         return None
-
