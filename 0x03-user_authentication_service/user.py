b69ddfe102323b9ac84bb4a70747c45d4d528037
MissTipo <dorine.a.tipo@gmail.com>
2023-05-10 12:28:04 +0300

    remove class constructor

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""User model."""
-
+"""Model User
+"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from typing import Optional
-
+from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Class User."""
+    """class User"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
@@ -18,15 +16,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __init__(
-            self,
-            email: str,
-            hashed_password: str,
-            session_id: Optional[str],
-            reset_token: Optional[str]):
-        """Instantiate User class."""
-        self.email = email
-        self.hashed_password = hashed_password
-        self.session_id = session_id
-        self.reset_token = reset_token
