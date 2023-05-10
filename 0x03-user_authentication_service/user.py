44bc912027f8ed21818735b44898c72df7744f8b
MissTipo <dorine.a.tipo@gmail.com>
2023-05-10 11:00:42 +0300

    add class constructor

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,6 +3,7 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from typing import Optional
 
 
 Base = declarative_base()
@@ -17,3 +18,15 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __init__(
+            self,
+            email: str,
+            hashed_password: str,
+            session_id: Optional[str],
+            reset_token: Optional[str]):
+        """Instantiates User class."""
+        email = self.email
+        hashed_password = self.hashed_password
+        session_id = self.session_id
+        reset_token = self.reset_token
